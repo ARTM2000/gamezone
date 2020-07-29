cf7b17cf44d8960ec0c3f818a4d583ded972b3ca
ARTM <tanoomandian204@gmail.com>
2020-07-30 01:03:44 +0430

    some things little updated

--- util/constants.js
+++
@@ -1,7 +0,0 @@
-const dbUser = process.env.DB_USER || "artm";
-const dbPassword = process.env.DB_PASSWORD || "aEDHGgaYFAIYlvci";
-const dbName = process.env.DB_NAME || "chatsInit";
-
-module.exports = {
-  db: `mongodb+srv://${dbUser}:${dbPassword}@cluster0-k82f8.mongodb.net/${dbName}?retryWrites=true&w=majority`,
-};
