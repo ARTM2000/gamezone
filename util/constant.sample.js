c04a2bd45c02cf1064af0d4e49b647d32c2bd8ce
ARTM <tanoomandian204@gmail.com>
2020-07-31 22:01:39 +0430

    some few update

--- util/constant.sample.js
+++ util/constant.sample.js
@@ -3,6 +3,6 @@ const dbPassword = process.env.DB_PASSWORD || "password";
 const dbName = process.env.DB_NAME || "databaseName";
 
 module.exports = {
-  db: `mongodb+srv://${dbUser}:${dbPassword}@cluster0-k82f8.mongodb.net/${dbName}?retryWrites=true&w=majority`,
+  db: `mongodb+srv://${dbUser}:${dbPassword}@cluster0-xxxxx.mongodb.net/${dbName}?retryWrites=true&w=majority`,
   jwtSecret: "your jsonWebToken secret key",
 };
